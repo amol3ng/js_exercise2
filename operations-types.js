904535463b5aa6b7e0ef917e89913afa27b5bb69
amol3ng <amohelamokhele@gmail.com>
2025-10-30 16:30:51 +0200

    Js intro

---
+++ operations-types.js
@@ -0,0 +1,8 @@
+let number1 = 10;
+let number2 = 5;
+let newNumber1 = number1.toSring();
+let sum = number1 + number2;
+let product = number1 * number2;
+console.log("number1 as a sting: " + number1);
+console.log("Sum: " = sum);
+console.log("Product: " + product);
